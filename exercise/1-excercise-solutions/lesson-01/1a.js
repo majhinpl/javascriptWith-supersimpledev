b38e594a9198833d88f9247ccf099904964aec68
nepstech <majhintech@gmail.com>
2024-02-09 16:29:27 +0300

    initialize the project

---
+++ exercise/1-excercise-solutions/lesson-01/1a.js
@@ -0,0 +1,3 @@
+//  1a. Use alert(...); to display 'Good morning!' in a popup.
+alert('Good morning!');
+console.log(alert);
---
+++ exercise/operator-precedence.js
@@ -0,0 +1,8 @@
+// * / are done first
+// + - are done after
+// * / have same priority it will calculate from left
+
+
+// 1. (...)
+// 2. * /
+// 3. + -
